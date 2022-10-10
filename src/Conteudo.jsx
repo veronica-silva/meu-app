bcff50b18b2d6b8fd57511bdf36874e5c7bb1fad
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2022-10-10 11:01:46 -0300

    isolando componentes
    por impor e export

---
+++ src/Conteudo.jsx
@@ -0,0 +1,22 @@
+const Conteudo = () => {
+  return (
+    <main>
+      <section>
+        <h2>Conteúdo...</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+          laboriosam tempore molestiae autem nam dolor excepturi numquam nemo
+          libero nulla, officiis fugit quis at ad maiores ratione cupiditate
+          necessitatibus impedit! Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Praesentium cumque facilis omnis deserunt nemo
+          veritatis cum. Sunt, esse veniam. Eos qui vero accusamus iusto earum
+          sed illo voluptate! Cum, sapiente. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. At quo voluptatum eos omnis et accusamus
+          provident, nostrum reprehenderit veniam nihil quae molestiae
+          voluptatibus amet! Quis ipsam commodi enim facilis veritatis?
+        </p>
+      </section>
+    </main>
+  );
+};
+export default Conteudo;
