039f7f32b844808fc319b768d4fd1e6066690a19
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2022-10-10 10:46:21 -0300

    exportando funções com arrow functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-
-function App() {
+const App = () => {
   return (
-  <div>
-    <h1>Olá, mundo!</h1>
-    <p>Aprendendo react!</p>
-  </div>);
-  
-}
+    <>
+      <header>
+        <h1>Inteface React</h1>
+        <nav>
+          <a href="">Home</a>
+          <a href="">Produtos</a>
+          <a href="">Servicos</a>
+        </nav>
+      </header>
+
+      <main>
+        <section>
+          <h2>Conteúdo...</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+            laboriosam tempore molestiae autem nam dolor excepturi numquam nemo
+            libero nulla, officiis fugit quis at ad maiores ratione cupiditate
+            necessitatibus impedit! Lorem ipsum, dolor sit amet consectetur
+            adipisicing elit. Praesentium cumque facilis omnis deserunt nemo
+            veritatis cum. Sunt, esse veniam. Eos qui vero accusamus iusto earum
+            sed illo voluptate! Cum, sapiente. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. At quo voluptatum eos omnis et
+            accusamus provident, nostrum reprehenderit veniam nihil quae
+            molestiae voluptatibus amet! Quis ipsam commodi enim facilis
+            veritatis?
+          </p>
+        </section>
+      </main>
+
+      <footer></footer>
+    </>
+  );
+};
 
 export default App;
---
+++ src/Cabecalho.jsx
@@ -0,0 +1,13 @@
+const Cabecalho = () => {
+  return (
+    <header>
+      <h1>Inteface React</h1>
+      <nav>
+        <a href="">Home</a>
+        <a href="">Produtos</a>
+        <a href="">Servicos</a>
+      </nav>
+    </header>
+  );
+};
+export default Cabecalho;
