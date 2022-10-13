5435b458e121f3487407ea80abd341455ffac110
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2022-10-13 11:06:49 -0300

    react hooks

--- src/components/Layouts/Cabecalho.jsx
+++ src/components/Layouts/Cabecalho.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import Menu from "../Menu";
-const estilo = {
-  color: "white",
-  backgroundColor: "darkgrey",
-};
+
 const Cabecalho = () => {
+  const exemplo2 = () => {
+    console.log("Exemplo 2");
+  };
+
+  const [titulo, setTitulo] = useState("Exemplo de State");
+  const atualizaTitulo = () => {
+    setTitulo(" O state do título foi alterado");
+  };
   return (
-    <header style={{ backgroundColor: "lightblue", textAlign: "center" }}>
-      <h1 style={estilo}>Inteface React</h1>
+    <header>
+      <button onClick={() => console.log("Exemplo1")}>Exemplo 1</button>
+      <button onClick={atualizaTitulo}>Exemplo 2</button>
+      <h1>{titulo}</h1>
       <Menu></Menu>
     </header>
   );
