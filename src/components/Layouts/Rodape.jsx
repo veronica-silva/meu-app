d4d5fcc63e31efa2ab89a1aeb9d042c0f713b28e
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2022-10-14 08:23:44 -0300

    Update Rodape.jsx

--- src/components/Layouts/Rodape.jsx
+++ src/components/Layouts/Rodape.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 const Rodape = () => {
+  const [estilos, setEstilos] = useState({
+    textAlign: "center",
+    textTransform: "uppercase",
+    border: "solid red 2px",
+  });
+  const esconder = () => setEstilos({ display: "none" });
+  const exibir = () => setEstilos({ display: "block" });
   return (
-    <footer>
-      <h2>Rodapé</h2>
-    </footer>
+    <>
+      <p onMouseOver={exibir}>Exibir</p>
+      <p onMouseOver={esconder}>Esconder</p>
+      <footer style={estilos}>
+        <h2>Rodapé</h2>
+      </footer>
+    </>
   );
 };
 export default Rodape;
